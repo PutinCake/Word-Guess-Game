e202d6a6724a4692156bde02f4120f1f06319bcb
PutinCake <zhiyemo09@gmail.com>
2018-04-26 16:58:31 -0700

    hw2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,43 +2,91 @@
 var wordsArray = ["simba", "thelionking", "tiger", "monkey", "goat", "dog", "timon", "pumbaa"];
 var computerWords = wordsArray[Math.floor(Math.random() * wordsArray.length)];
 var wins = 0;
-var loss
+var winCounter = 0;
+var loss = 0;
 var guessesLeft = 9;
-var guessedRightWord = [];
-var guessedWrongWord = [];
+var rightLetter = [];
+var wrongLetter = [];
 var guessedWord = [];
+var userGuesses = [];
 console.log(computerWords);
 
+//function startGame(){
 
-function guessedWordLine() {
-    for(var i = 0; i < computerWords.length; i++) {
-        guessedWord.push('_');
+    function guessedWordLine() {
+        for(var i = 0; i < computerWords.length; i++) {
+            guessedWord.push('_');
+        }
     }
-    return guessedWord;
-}
 
-console.log(guessedWordLine());
-//get users guess
-document.addEventListener('keypress', (event)=> {
-    var keycode = event.keyCode;
-    var keyword = String.fromCharCode(keycode);
-    //console.log(keyword);
+    guessedWordLine();
 
-    if(computerWords.indexOf(keycode) > -1){
-        //console.log(true);
-        computerWords.push(keyword);
+    function updateDisplay(){
+        document.querySelector('#word-blanks').textContent = guessedWord.join(' ');
+    }
 
-        console.log(guessedWord);
-        guessedWordLine[computerWords.indexOf(keyword)] = keyword;
+    
+    // function reset(){
+    //     computerWords = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+    //     guessedWordLine();
+    // }
 
+        //onkeyup function
+        document.onkeyup = function(event){
+            userGuesses = event.key;
+            console.log(userGuesses);
+            for(var i = 0; i < computerWords.length; i++){
+                if(computerWords[i] === userGuesses){
+                    guessedWord[i] = userGuesses;
+                    updateDisplay();
+                    winCounter++;
+                    winOrLose();
+                } 
+            }
+            if (!guessedWord.includes(userGuesses)) {
+                wrongLetter.push(userGuesses);
+            }
+            guessesLeft--;            
+            winOrLose();
+        }
+
+        function winOrLose(){
+            if(winCounter === computerWords.length){
+                alert('Winner!');
+                wins++;
+                console.log(wins);
+            } else {
+                if (guessesLeft === 0) {
+                    alert('Loser!');
+                }
+            }
 
     }
-});
+/*
+    var html = 
+    "<h1>PRESS ANY KEY TO GET STRATED!!!</h1>" +
+    "<p>Wins:"  + wins + "</p>"+
+    "<p>Word:</p>" +
+    "<p>" + computerWords + "</p>" +
+    "<p>Numbers of Guesses left: " + guessesLeft + "</p>"
+    "<P>Letters Already Guessed: " + guessedWord + "</P>";
+
+    document.querySelector("#game").innerHTML = html;
+*/
+/*
+    var html = 
+	"<h1> The Psychic Game ---Start!!!</h1>" +
+	"<p>Guess what letter I'm thinking of</p>" +
+	"<p>Wins: " + wins + "</p>" +
+	"<p>Losses: " + losses + "</p>" +
+	"<p>Guesses Left: " + numGuesses + "</p>" +
+	"<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
+
+    document.querySelector("#game").innerHTML = html;
+*/
 
 
-
-
-var wins = 0;
-var guessesLeft = 9;
+//========================================================================
+//start game!!!!
 
 
